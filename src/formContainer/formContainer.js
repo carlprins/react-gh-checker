c6fc2d8fb6c706efcdfbfd57a44f830b714ba189
carlprins <54782746+carlprins@users.noreply.github.com>
2019-09-30 14:07:10 +1000

    Update formatting

--- src/formContainer/formContainer.js
+++ src/formContainer/formContainer.js
@@ -18,44 +18,65 @@ const section1 = [
         name: "Bill End Date",
         type: "date",
         options: []
-    }
-]
-
-const section2 = [
-    {
-        id: 1,
+    }, {
+        id: 4,
+        name: "Opening Balance",
+        type: "number",
+        options: []
+    }, {
+        id: 5,
+        name: "Closing Balance",
+        type: "number",
+        options: []
+    },{
+        id: 4,
         name: "Period Start Date",
         type: "date",
         options: []
 
     }, {
-        id: 2,
+        id: 5,
         name: "Period End Date",
         type: "date",
         options: []
     }, {
-        id: 3,
+        id: 6,
         name: "Peak Usage",
         type: "number",
         options: []
     }, {
-        id: 4,
+        id: 7,
         name: "Peak Rate",
         type: "number",
         options: []
     }
 ]
 
+const section2 = [
+    
+]
+
 class FormContainer extends React.Component {
     constructor(props){
         super(props)
+        this.state = { 
+            billTotal: 10,
+            OpeningBalance: 0,
+            ClosingBalance: 0
+        }
+        this.handleUpdate = this.handleUpdate.bind(this);
+    }
+    handleUpdate(value) {
+        this.setState( {billTotal: value})
     }
     render(){
         return (
             <div className="FormContainer">
                 <h1>{this.props.formTitle}</h1>
-                <FormSection sectionName="Basic Information" sectionInputs={section1} />
-                <FormSection sectionName="Period Information" sectionInputs={section2} />
+                <p>Bill Total</p>
+                <p>{this.state.billTotal}</p>
+                <FormSection sectionName="Basic Information" sectionInputs={section1} onChange={this.handleUpdate} />
+                
             </div>
             
             
--- src/formSection/formSection.js
+++ src/formSection/formSection.js
@@ -13,13 +13,13 @@ class FormSection extends React.Component {
                 <h1>{this.props.sectionName}</h1>
                 {this.props.sectionInputs.map(input => {
                     if (input.type === "text") {
-                        return <FormInput name={input.name} type={input.type} />
+                        return <FormInput name={input.name} type={input.type} key={input.id} />
                     } else if (input.type === "dropdown") {
-                        return <FormSelect name={input.name} options={input.options} />
+                        return <FormSelect name={input.name} options={input.options} key={input.id} />
                     } else if (input.type === "date") {
-                        return <FormInput name={input.name} type={input.type} />
+                        return <FormInput name={input.name} type={input.type} key={input.id} />
                     } else if (input.type === "number") {
-                        return <FormInput name={input.name} type={input.type} />
+                        return <FormInput name={input.name} type={input.type} key={input.id} />
                     }
                 })}
             </div>
